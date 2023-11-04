d5b7533ebe7ede5d6a4c76cf4140df0419328ab6
Aminur Rahman <dev.aminur@gmail.com>
2023-11-05 00:52:13 +0600

    Login routes created complete

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Register from "../Pages/Register/Register";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
 
 const Routes = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const Routes = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
       },
       {
         path: "/register",
